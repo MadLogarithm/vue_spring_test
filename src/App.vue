d43156fa65edd46d7ccd9213ceeda13f7b794d3f
MadLogarithm <1141519626@qq.com>
2024-01-16 16:11:19 +0800

    feat(): add tableData processing function.

--- src/App.vue
+++ src/App.vue
@@ -2,6 +2,11 @@
   <div id="app">
     <button @click="getHello">Get Hello</button>
     <p>{{ responseMessage }}</p>
+    <el-table :data="tableData" stripe style="width: 100%">
+      <el-table-column prop="id" label="id" width="180"></el-table-column>
+      <el-table-column prop="name" label="姓名" width="180"></el-table-column>
+      <el-table-column prop="email" label="邮箱"></el-table-column>
+    </el-table>
   </div>
 </template>
 
@@ -11,20 +16,31 @@ import axios from 'axios';
 export default {
   data() {
     return {
-      responseMessage: ''
+      responseMessage: '',
+      tableData: []
     };
   },
   methods: {
     getHello() {
       // 发送GET请求到后端的"/hello"路径
-      axios.get('http://localhost:8080/hello') // 请替换为你的后端地址和端口
+      axios.get('http://localhost:8080/getUser') // 请替换为你的后端地址和端口
         .then(response => {
           this.responseMessage = response.data;
+          this.tableData = this.parseData(response.data);
         })
         .catch(error => {
           this.responseMessage = error;
           console.error('Error fetching hello:', error);
         });
+    },
+    parseData(data) {
+      return data.map(item => {
+        return {
+          id: item[0],
+          name: item[1],
+          email: item[2]
+        };
+      });
     }
   }
 };
